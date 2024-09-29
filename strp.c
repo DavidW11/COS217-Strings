e2342c3fd0b1e3137ef97d3e870ffac9d1f8457e
David Wang <davidwang7725@gmail.com>
2024-09-29 13:51:10 -0400

    fix const bugs in strip

--- strp.c
+++ strp.c
@@ -10,7 +10,7 @@
 /* returns length of string pointed to by s,
 not including the null character. */
 size_t Str_getLength(const char *s) {
-    char *end = s;
+    const char *end = s;
 
     assert(s!=NULL);
     while (*end != '\0') {
@@ -23,7 +23,7 @@ size_t Str_getLength(const char *s) {
 /* copies string pointed to by s2 to pointer s1.
 returns s1 (pointer of the copy). */
 char *Str_copy(char *s1, const char *s2) {
-    char *currentChar = s2;
+    const char *currentChar = s2;
     assert(s1!=NULL && s2!=NULL);
     while (*currentChar != '\0') {
         *s1 = *currentChar;
@@ -39,7 +39,7 @@ the string pointed to by s1.
 returns s1 (pointer to final string). */
 char *Str_concat(char *s1, const char *s2) {
     char *end = s1;
-    char *currentChar = s2;
+    const char *currentChar = s2;
 
     assert(s1!=NULL && s2!=NULL);
     /* iterate to end of string pointed to by s1 */
@@ -59,8 +59,8 @@ char *Str_concat(char *s1, const char *s2) {
 depending if string pointed to by s1 is less than, equal to, or 
 greater than string pointed to by s2. */
 int Str_compare(const char *s1, const char *s2) {
-    char *c1 = s1;
-    char *c2 = s2;
+    const char *c1 = s1;
+    const char *c2 = s2;
 
     assert(s1!=NULL && s2!=NULL);
     while (*c1 != '\0' && *c2 != '\0') {
@@ -76,9 +76,9 @@ int Str_compare(const char *s1, const char *s2) {
 /* searches string pointed to by s1 for string pointed to by s2, 
 returns pointer to start of substring if it exists and NULL otherwise */
 char *Str_search(const char *s1, const char *s2) {
-    char *startSubstring = s1;
-    char *c1 = s1;
-    char *c2 = s2;
+    const char *startSubstring = s1;
+    const char *c1 = s1;
+    const char *c2 = s2;
 
     assert(s1!=NULL && s2!=NULL);
     if (*c2=='\0') {
